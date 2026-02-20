c77e40b4a92d63a40adb3d8d15759f4eb29a9baa
TJ Evans <tj.evansarreguin@gmail.com>
2026-02-20 14:30:57 -0500

    UPDATES 02/20
    Co-authored-by: Lee, Daniel <djlee@exeter.edu>

--- cube.py
+++ cube.py
@@ -1,8 +1,24 @@
+import sys
 import pygame
+import pygame.locals
+
+from cube import Cube
 
 class Cube:
-    def __init__(self, surface: pygame.Surface) -> None:
+    def __init__(self, surface: pygame.Surface, x: float, y: float, size: float) -> None:
+        self.surface = surface
+        self.x, self.y = x, y
+        self.size = size
+        self.vy = 0 # velocity
+        self.ay = 5 #acceleration
+
+    def jump(self):
+        self.vy = -10
 
     def update(self) -> None:
-        keys_held = pygame.key.get_pressed()
-        
+        self.vy += self.ay
+                
+
+    def display(self) -> None:
+
+        pygame.draw.rect(self.surface, "#FFDD40", (self.x, self.y, self.size, self.size))
--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import sys
 import pygame
 import pygame.locals
 
+from cube import Cube
+
+def player():
+    
 
 def main():
-    
+    fps = 60
+    fps_clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 600))
+
+    pygame.draw.rect(self.surface, "#FFDD40", (self.x, self.y, self.size, self.size))
+
+    state = "start"
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.locals.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.locals.KEYDOWN:
+                if event.key == pygame.K_SPACE: #make text display "press space to continue"
+                    if state == "start":
+                        state = "game"
+                    elif state == "game":
+
+                        
+    if state == "game":
+        screen.fill("#58A1F0")
+    if state == "death":
+        screen.fill("#e00c0c")  
